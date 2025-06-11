227c2b8cd45c718962b89bc62256716480987bbb
‘Kanav <kanav29sharma@gmail.com>
2025-06-11 20:46:55 +0530

    cors allow all origins

--- backend/index.ts
+++ backend/index.ts
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app: Express = express();
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: "*",
   methods: ["GET", "POST"]
 }));
 
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
