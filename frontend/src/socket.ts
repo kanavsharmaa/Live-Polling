9adcf22dcc0045f9fec217e24ce8b93622b47a7c
‘Kanav <kanav29sharma@gmail.com>
2025-06-11 14:12:51 +0530

    env variable in frontend

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,10 +1,7 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL =
-  import.meta.env.MODE === "production"
-    ? undefined
-    : "http://localhost:3001";
+const URL = import.meta.env.VITE_BACKEND_URL;
 
 export const socket = io(URL, {
   autoConnect: false,
